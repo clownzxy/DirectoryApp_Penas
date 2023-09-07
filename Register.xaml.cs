7b1381b80f4996b2138a565960650fe78401c31a
NORWEN PEnAS <norwen.penas.21@usjr.edu.ph>
2023-09-07 16:40:26 +0800

    testing for picker

--- Register.xaml.cs
+++ Register.xaml.cs
@@ -1,69 +1,12 @@
-using System.Collections.ObjectModel;
-
 namespace DirectoryApp;
 
 public partial class Register : ContentPage
 {
-    private ObservableCollection<String> _schoolProgram;
-    private ObservableCollection<String> _course;
-
-    public ObservableCollection<String> SchoolProgram
-    {
-        get { return _schoolProgram; }
-        set { _schoolProgram = value; }
-    }
-
-    public ObservableCollection<String> Course
-    {
-        get { return _course; }
-        set { _course = value; }
-    }
-
+    
 
     public Register()
 	{
 		InitializeComponent();
-
-        SchoolProgram = new ObservableCollection<String>()
-        {
-            "DEFAULT",
-            "SOE",
-            "SBM"
-        };
-
-        Course = new ObservableCollection<string>
-        {
-            "DEFAULT"
-        };
-
-        picker.ItemsSource = SchoolProgram;
-        coursePicker.ItemsSource = Course;
-        picker.SelectedItem = "DEFAULT";
-        coursePicker.SelectedItem = "DEFAULT";
-        picker.SelectedIndexChanged += Picker_SelectedIndexChanged;
-        BindingContext = this;
-    }
-
-    
-
-    
-
-    private void Picker_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        var picker = (Picker)sender;
-        int selectedIndex = picker.SelectedIndex;
-
-        Course.Clear();
-        if(selectedIndex == 1 ) 
-        {
-            Course.Add("BSCPE");
-            Course.Add("BSCE");
-            Course.Add("BSCE");
-        }else if(selectedIndex == 2) 
-        {   
-            Course.Add("BSOM");
-            Course.Add("BSACC");
-        }
     }
 
 }
